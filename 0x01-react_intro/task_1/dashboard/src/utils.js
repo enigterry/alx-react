59d289b3aa423e93486cbe3661649b8b0dccc600
enigterry <terryonaiwu@gmail.com>
2022-05-28 15:40:43 +0000

    react

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+export const getFullYear = () => {
+  let current_year = new Date().getFullYear();
+  return current_year;
+}
+export const getFooterCopy = (isIndex) => {
+  if(isIndex === true) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
